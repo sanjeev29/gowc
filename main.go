8239c531d4cf9b9fdeb1825069224a7d81cacd10
sanjeev40 <sanjeevraichur29@gmail.com>
2024-05-10 15:18:19 -0400

    Read number of bytes in a file

--- main.go
+++ main.go
@@ -1,7 +1,64 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func readBytes(file *os.File) int {
+	numberOfBytes := 0
+
+	reader := bufio.NewReader(file)
+
+	for {
+		_, size, err := reader.ReadRune()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+
+			log.Fatalln(err)
+		}
+
+		numberOfBytes += size
+	}
+
+	return numberOfBytes
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	var bytes bool
+
+	flag.BoolVar(&bytes, "c", false, "number of bytes")
+
+	flag.Parse()
+
+	filename := flag.CommandLine.Arg(0)
+
+	_, err := os.Stat(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Open file
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Close file
+	defer file.Close()
+
+	var numberOfBytes int
+
+	if bytes {
+		numberOfBytes = readBytes(file)
+	}
+
+	// Print number of bytes
+	fmt.Printf("  %d %s\n", numberOfBytes, filename)
 }
